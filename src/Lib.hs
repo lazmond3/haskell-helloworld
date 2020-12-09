560710bd458d02ba32008b120914c94c8679d394
Your Name <you@example.com>
2020-12-09 21:16:37 +0900

    update format

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,15 @@
 module Lib
-    ( someFunc 
-    ) where
+  ( someFunc,
+  )
+where
+
 import Data.Function ((&))
 
 someFunc :: IO ()
 
-primes = filterPrime [2..]
-    where filterPrime (p:xs) = 
-            p : filterPrime [x | x <- xs, x `mod` p /= 0]
+primes = filterPrime [2 ..]
+  where
+    filterPrime (p : xs) =
+      p : filterPrime [x | x <- xs, x `mod` p /= 0]
 
-someFunc = take 9 primes  & show & putStrLn
-
+someFunc = take 9 primes & show & putStrLn
