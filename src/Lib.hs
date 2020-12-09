114bd6ad296a3c64c239642be2a3a57a978c0112
Your Name <you@example.com>
2020-12-09 22:44:15 +0900

    update with case

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,4 +12,14 @@ primes = filterPrime [2 ..]
     filterPrime (p : xs) =
       p : filterPrime [x | x <- xs, x `mod` p /= 0]
 
-someFunc = take 9 primes & show & putStrLn
+convert_list_to_string:: [Integer] -> String
+convert_list_to_string (a:as) = show a ++ 
+    case length as of
+        0 -> ". finished"
+        _ -> ", " ++ convert_list_to_string as
+
+
+someFunc = take 9 primes &
+            convert_list_to_string & 
+            -- show & 
+            putStrLn
