18877199e8b31a155a8008b329259eaa86b6ee5e
Your Name <you@example.com>
2020-12-14 13:25:43 +0900

    update nyshuffle

--- src/Random.hs
+++ src/Random.hs
@@ -7,6 +7,12 @@ import System.Random
 
 nyshuffle [] = return []
 nyshuffle xs = do
-  n <- getStdRandom $ randomR (0, length xs - 1) :: IO Int
-  xs' <- nyshuffle $ take n xs ++ drop (n + 1) xs
+  n <- getStdRandom $ randomR (0, length xs -1)
+  print n -- ランダムに1文字取り出す その index となる。
+  xs' <- nyshuffle $ take n xs ++ drop (n + 1) xs -- n番目を落として先頭に加える、という関数
   return $ (xs !! n) : xs'
+
+-- nyshuffle xs = do
+--   n <- getStdRandom $ randomR (0, length xs - 1) :: IO Int
+--   xs' <- nyshuffle $ take n xs ++ drop (n + 1) xs
+--   return $ (xs !! n) : xs'
